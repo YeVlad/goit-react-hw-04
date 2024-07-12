71cedae6d005b7496d15340dab9e22e79e7cafc8
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-07-13 00:23:42 +0200

    fix 2

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import { Audio } from 'react-loader-spinner';
 
-type Props = {
-  height:  string; 
-  width:  string; 
-  color: string; 
-  ariaLabel: string; 
-};
-
-const Loader: React.FC<Props> = ({
-  height,
-  width,
-  color = 'green',
-  ariaLabel = 'three-dots-loading',
-}) => {
+const Loader: React.FC = () => {
   return (
     <div className="loading-area">
       <Audio
-        height={height}
-        width={width}
-        color={color}
-        ariaLabel={ariaLabel}
+        height="100"
+        width="100"
+        color="blue"
+        ariaLabel="Loading"
       />
     </div>
   );
